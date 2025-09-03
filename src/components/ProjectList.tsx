02683df2cb8821155f9cb33579a062088abd4da8
Fernando <fernandocr019@gmail.com>
2025-09-03 15:04:54 -0300

    TSK1 fix: Adiciona projeto de bot de relatório

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -24,6 +24,13 @@ export default function ProjectsList() {
         "Desenvolvi um agent de IA utilizando LangChain e o protocolo MCP, eles estão equipado com um conjunto de ferramentas para treinamento de modelos de machine learning e realização de previsões a partir de uma base de dados pré-definida. O agent avalia os modelos treinados, identifica aquele com melhor desempenho e o retorna para ser utilizado em uma análise posterior, com novos dados que serão enviados pelo usuário.",
       image: require("../assets/AIAgent.jpg"),
     },
+    {
+      id: 3,
+      title: "Bot de Relatório",
+      description:
+        "Desenvolvi um Bot para Telegram capaz de monitorar mensagens e, mediante o comando /report, gerar relatórios automatizados de logs do sistema. Essa solução auxilia no processo de auditoria, proporciona maior transparência operacional e facilita o acompanhamento em tempo real do comportamento do aplicativo onde for implementado.",
+      image: require("../assets/BotTelegram.png"),
+    },
   ];
 
   return (
